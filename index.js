538f847712348a6bb4594ddc036e259cd6b43262
Parthib Mukherjee <parthibmukherheee@gmaile.com>
2023-03-18 21:30:04 +0530

    added colors from button and adjustable grid

--- index.js
+++ index.js
@@ -1,29 +1,92 @@
 
 "use strict";
 
-function board() {
-    let  container = document.querySelector("#container");
+let container = document.querySelector("#container");
+const gridLine = document.querySelector(".gridLine");
+//let color = document.querySelector(".color");
+let color = "white";
 
-    container.style.gridTemplateColumns = `repeat(16, 1fr)`;
-    container.style.gridTemplateRows = `repeat(16, 1fr)`;
-    for(let i = 0; i < 16*16; i++) {
+//let square;
+
+board(32);
+
+
+
+
+
+function board(size) {
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
         //myGrid.push(createDiv());
         //container.appendChild(myGrid[i]);
         let square = document.createElement("div");
 
+
         square.style.backgroundColor = "cyan";
-        square.style.border = "1px solid black";
+
+        gridLine.addEventListener("click", () => {
+            if (gridLine.value === "ON")
+                square.style.border = "";
+
+            else {
+                square.style.border = "1px solid black";
+            }
+
+        });
+
+        square.addEventListener("mouseover",squareColor);
+
+
+
+
 
 
         container.insertAdjacentElement("beforeend", square);
-        square.addEventListener("mouseover",() => {
-            square.style.background = "yellow";
-        });
+        /*function (color){
 
+        }*/
 
 
     }
 }
+function changeSize(size){
+    if(size > 0 && size <= 100){
+        while(container.firstChild){
+            container.removeChild(container.firstChild);
+        }
+        board(size);
+    }
+    else {
+      alert("Please choose a proper number")
+    }
+
+}
+
+function squareColor(){
+    this.style.backgroundColor = color;
+}
+function changeColor(option){
+    if(option === "OFF"){
+
+    }else{
+        color = option;
+    }
+}
+function changeValue(){
+    if(gridLine.value === "ON"){
+        gridLine.value = "OFF";
+    }else{
+        gridLine.value = "ON"
+    }
+
+}
+
+
+
+
 
         //gridStyle(gridDiv);
 
